f7b40bf986d251aec1c5510adbd308b6c6e0bc6c
Sven Joder <sj@pmdevelopment.de>
2017-06-15 14:33:03 +0200

    packagist stuff

---
+++ PMGermanHolidayBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PM\Bundle\GermanHolidayBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PMGermanHolidayBundle extends Bundle
+{
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # german-holiday-bundle
+
+Get german holidays as symfony2 bundle.
+
+-- 
+
+Deutsche Feiertage für Symfony2 als Bundle.
